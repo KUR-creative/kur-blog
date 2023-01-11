79b664960c32b6fdf471128eab9b334c63d5847a
kur <rhdnfka94@gmail.com>
2023-01-11 17:19:37 +0900

    Use id when no title(Fix no tags links order)

--- src/kur/blog/look/tags.clj
+++ src/kur/blog/look/tags.clj
@@ -8,12 +8,18 @@
        ":" 3000
        "/" post-id))
 
-(defn post-link-li
-  [{:keys [id title]}]
-  [:li (link-to (post-url id) (if title title id))])
+(defn title-or-id [{:keys [id title] :as post}]
+  (if title
+    title
+    id))
 
+(defn post-link-li
+  [post]
+  [:li (link-to (-> post :id post-url) (title-or-id post))])
+
+(get {1 1 2 2} 0 10)
 (defn tags-summary [tag:posts no-tags-posts]
-  (let [sort-by-title #(sort-by :title %)
+  (let [sort-by-title #(sort-by title-or-id %)
         tag:posts (into (sorted-map) (update-vals tag:posts sort-by-title))
         no-tags-posts (sort-by-title no-tags-posts)]
     (list (mapcat (fn [[tag posts]]
