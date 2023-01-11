db44247be1a7f46ea0338743ff5accd199cf0335
kur <rhdnfka94@gmail.com>
2023-01-12 07:09:48 +0900

    Split to site(calc) and run!(side effect)

--- src/kur/blog/writer.clj
+++ src/kur/blog/writer.clj
@@ -7,32 +7,27 @@
             [kur.blog.look.home :as look-home]
             [kur.util.file-system :as uf]))
 
-(defn post-set [md-dir]
-  (->> md-dir uf/path-seq (map post/post) set))
+#_(defn post-set [md-dir]
+    (->> md-dir uf/path-seq (map post/post) set))
 
-#_(defn site [])
+(defn site
+  "Return ([html-path page-html] ...). posts shuold be vector."
+  [posts html-dir]
+  (letfn [(html-path [fname] (str (fs/path html-dir fname)))]
+    (map vector
+         (conj (mapv #(html-path (post/html-file-name %)) posts)
+               (html-path "tags.html")
+               (html-path "home.html"))
+         (conj (mapv #(look-post/html nil (:text %)) posts)
+               (look-tags/html (tags/tag:posts posts)
+                               (filter #(not (tags/has-tags? %)) posts))
+               (look-home/html (sort-by :id ; id means creation time
+                                        posts))))))
 
 ;;
 (comment
   (def md-dir "test/fixture/blog-root/blog-md/")
   (def html-dir "test/fixture/blog-root/tmp-html/")
 
-  ;; post 출력
-  (def posts (post-set md-dir))
-  (def post-htmls
-    (map #(look-post/html nil (:text %)) posts))
-  (def post-html-paths
-    (map #(str (fs/path html-dir (post/html-file-name %))) posts))
-
-  (run! (fn [[path html]] (spit path html))
-        (map vector post-html-paths post-htmls))
-
-  ;; tags 출력
-  (spit (str (fs/path html-dir "tags.html"))
-        (look-tags/html (tags/tag:posts posts)
-                        (filter #(not (tags/has-tags? %)) posts)))
-
-  ;; home 출력
-  (spit (str (fs/path html-dir "home.html"))
-        (look-home/html (sort-by :id posts))) ; id means creation time
-  )
+  (def a-site (site (->> md-dir uf/path-seq (map post/post)) html-dir))
+  (run! (fn [[path html]] (spit path html)) a-site))
