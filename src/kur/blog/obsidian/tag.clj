6e52b8a6f469582b87a20ab523f00b47897cb227
kur <rhdnfka94@gmail.com>
2023-01-17 14:13:31 +0900

    Fix: purely numeric tag is not allowed
    https://help.obsidian.md/How+to/Working+with+tags#Allowed+characters

--- src/kur/blog/obsidian/tag.clj
+++ src/kur/blog/obsidian/tag.clj
@@ -12,10 +12,11 @@
   (re-matches tag-character-regex s))
 
 (s/def ::tag
-  (s/with-gen (s/and string? only-allowed-chars?)
+  (s/with-gen (s/and string? only-allowed-chars? #(nil? (parse-long %)))
     (fn [] (g/such-that only-allowed-chars?
                         (string-from-regexes ascii* hangul*)
                         200))))
 
 (comment
-  (g/sample (s/gen ::tag)))
+  (g/sample (s/gen ::tag))
+  (s/valid? ::tag "012"))
