8b5c635908300e7b9b26052b048df8c67f14d223
kur <rhdnfka94@gmail.com>
2023-01-17 10:44:16 +0900

    Add obsidian tag spec for my blog

---
+++ src/kur/blog/obsidian/tag.clj
@@ -0,0 +1,21 @@
+(ns kur.blog.obsidian.tag
+  (:require
+   [clojure.test.check.generators :as g]
+   [clojure.spec.alpha :as s]
+   [kur.util.generator :refer [string-from-regexes]]
+   [kur.util.regex :refer [ascii* hangul*]]))
+
+(def tag-character-regex
+  "See https://help.obsidian.md/How+to/Working+with+tags#Allowed+characters"
+  #"[a-z|A-Z|0-9|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\-_/]+")
+(defn only-allowed-chars? [s]
+  (re-matches tag-character-regex s))
+
+(s/def ::tag
+  (s/with-gen (s/and string? only-allowed-chars?)
+    (fn [] (g/such-that only-allowed-chars?
+                        (string-from-regexes ascii* hangul*)
+                        200))))
+
+(comment
+  (g/sample (s/gen ::tag)))
