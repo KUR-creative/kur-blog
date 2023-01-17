720152370505686009928975a1c32251b681593b
kur <rhdnfka94@gmail.com>
2023-01-17 14:05:04 +0900

    Move post.frontmatter below obsdian.x, Rename fns

---
+++ src/kur/blog/obsidian/frontmatter.clj
@@ -0,0 +1,26 @@
+(ns kur.blog.obsidian.frontmatter
+  "Obsidian markdown frontmatter parser
+   
+   An md doc shape:
+         <------------ cap
+   ---   <------------ head
+   tags: [aa, bb]  <-- frontmatter
+   ---   <------------ foot
+   txt.. <------------ shoe"
+  (:require [clj-yaml.core :as yaml]
+            [clojure.string :as str]))
+
+(defn parse-yaml
+  "Return yaml parsed data. if error occurs, return nil"
+  [s]
+  (try
+    (yaml/parse-string s)
+    (catch Exception _
+      nil)))
+
+(defn parse [md]
+  (let [[trimmed-cap rest] (str/split (str/triml md) #"---\n" 2)
+        [frontmatter-str shoe] (when rest (str/split rest #"\n---" 2))]
+    (if (or (not= trimmed-cap "") (nil? rest) (nil? shoe))
+      {:frontmatter nil :body md}
+      {:frontmatter (parse-yaml frontmatter-str) :body shoe})))
--- test/unit/kur/blog/obsidian/frontmatter_test.clj
+++ test/unit/kur/blog/obsidian/frontmatter_test.clj
@@ -1,4 +1,4 @@
-(ns kur.blog.page.post.frontmatter-test
+(ns kur.blog.obsidian.frontmatter-test
   "Markdown frontmatter parser test"
   (:require
    [clojure.spec.alpha :as s]
@@ -7,7 +7,7 @@
    [clojure.test.check.properties
     :refer [for-all] :rename {for-all defp}]
    [com.gfredericks.test.chuck.generators :as g']
-   [kur.blog.page.post.frontmatter :refer :all]
+   [kur.blog.obsidian.frontmatter :as frontmatter]
    [kur.util.regex :refer [common-whitespace*]]
    [clj-yaml.core :as yaml]))
 
@@ -20,7 +20,7 @@
 
 ;; frontmatter
 (def gen-yaml-io (g/fmap yaml/generate-string g/any-printable-equatable))
-(def gen-non-yaml (g/such-that #(and (not (yaml-parse %))
+(def gen-non-yaml (g/such-that #(and (not (frontmatter/parse-yaml %))
                                      (not (.contains % "---"))
                                      (not (.endsWith % "--"))
                                      (not (.endsWith % "-")))
@@ -49,19 +49,19 @@
   (defp [m (nil-all-case gen-not-ws-cap (s/gen #{head ""})
                          (g/one-of [gen-yaml-io gen-non-yaml])
                          (s/gen #{foot ""}) gen-shoe)]
-    (= (obsidian (:input m)) (dissoc m :input))))
+    (= (frontmatter/obsidian (:input m)) (dissoc m :input))))
 
 (defspec head-is-not-exists-then-fm-is-nil 100
   (defp [m (nil-all-case gen-not-ws-cap (g/return "")
                          (g/one-of [gen-yaml-io gen-non-yaml])
                          (s/gen #{foot ""}) gen-shoe)]
-    (= (obsidian (:input m)) (dissoc m :input))))
+    (= (frontmatter/obsidian (:input m)) (dissoc m :input))))
 
 (defspec no-foot-then-fm-is-nil 100
   (defp [m (nil-all-case gen-ws-cap (s/gen #{head ""})
                          (g/one-of [gen-yaml-io gen-non-yaml])
                          (g/return "") gen-no-foot-shoe)]
-    (= (obsidian (:input m)) (dissoc m :input))))
+    (= (frontmatter/obsidian (:input m)) (dissoc m :input))))
 
 (def head-foot-exists
   (g/let [cap gen-ws-cap
@@ -69,22 +69,22 @@
           s gen-shoe]
     (let [inp (str cap "---\n" fm "\n---" s)]
       {:parts [cap "---\n" fm "\n---" s]
-       :input inp, :frontmatter (yaml-parse fm), :body s})))
+       :input inp, :frontmatter (frontmatter/parse-yaml fm), :body s})))
 (defspec head-foot-exists-case 1000
   (defp [m head-foot-exists]
-    (= (obsidian (:input m)) (select-keys m [:frontmatter :body]))))
+    (= (frontmatter/obsidian (:input m)) (select-keys m [:frontmatter :body]))))
 
 ;;
 (comment
-  (obsidian "---\n@----") (println "---\n@----")
-  (obsidian "---\n0\n---asdf")
-  (obsidian "---\n12345 234\n---asdf")
-  (obsidian "---\n- {'---': 0}\n---") (println "---\n- {'---': 0}\n---")
-  (obsidian "---\ntags: [---]\n---")
-  (obsidian "---\ntags: [가나다, 라마]\n---테트\n테스트")
-  (obsidian "---\n---aaa")
-  (obsidian "---\n---")
-  (obsidian "---\n0\n\n---")
+  (frontmatter/obsidian "---\n@----") (println "---\n@----")
+  (frontmatter/obsidian "---\n0\n---asdf")
+  (frontmatter/obsidian "---\n12345 234\n---asdf")
+  (frontmatter/obsidian "---\n- {'---': 0}\n---") (println "---\n- {'---': 0}\n---")
+  (frontmatter/obsidian "---\ntags: [---]\n---")
+  (frontmatter/obsidian "---\ntags: [가나다, 라마]\n---테트\n테스트")
+  (frontmatter/obsidian "---\n---aaa")
+  (frontmatter/obsidian "---\n---")
+  (frontmatter/obsidian "---\n0\n\n---")
 
   (do
     (cap-is-not-ws-then-fm-is-nil)
