58bd3d338ecbf0823395198b257f7c90a115ae9d
kur <rhdnfka94@gmail.com>
2023-01-18 09:01:48 +0900

    Add runners and basic test loop

--- test/integration/kur/blog_test/spbt.clj
+++ test/integration/kur/blog_test/spbt.clj
@@ -2,10 +2,13 @@
   (:require
    [babashka.fs :as fs]
    [clojure.spec.alpha :as s]
+   [clojure.test.check.clojure-test :refer [defspec]]
    [clojure.test.check.generators :as g]
-   [kur.blog.page.post.name :as name]
-   [kur.blog.obsidian.tag :as tag]
+   [clojure.test.check.properties
+    :refer [for-all] :rename {for-all defp}]
    [kur.blog.obsidian.frontmatter-test :as fmt]
+   [kur.blog.obsidian.tag :as tag]
+   [kur.blog.page.post.name :as name]
    [kur.util.file-system :as uf]
    [kur.util.generator :refer [string-from-regexes]]
    [kur.util.regex :refer [ascii* common-whitespace* hangul*]]))
@@ -29,7 +32,7 @@
                                   (fmt/gen-tags-yaml tags)]))]
     {:tags tags :frontmatter frontmatter}))
 
-(defn gen-md-files [dir]
+(defn gen-md-file [dir]
   (g/let [fname gen-post-name
           {:keys [tags frontmatter]} gen-tags-and-frontmatter
           md-text gen-md-text]
@@ -47,9 +50,40 @@
     (g/fmap #(assoc % :kind read) (g/elements md-files)))
 (def gen-update-sys ;; NOTE: with timing에서는 제거해야..
   (g/return {:kind :upd-sys}))
+
 (defn gen-ops [md-files]
+  (def md-files md-files)
   (g/vector (g/one-of [(gen-create md-files) (gen-delete md-files)
                        gen-update-sys])))
+
+;; Runners
+(defn next-model [op model]
+  (case (:kind op)
+    :create  (assoc model (:path op) (:text op))
+    :delete  (dissoc model (:path op))
+    :upd-sys model))
+
+(defn next-actual
+  "The actual state is a directory in file system!"
+  [op]
+  (case (:kind op)
+    :create  (spit (:path op) (:text op))
+    :delete  (fs/delete-if-exists (:path op))
+    :upd-sys :upd-sys))
+
+;; Test
+(defspec test-without-timing
+  (let [md-dir "test/fixture/spbt/md"
+        html-dir "test/fixture/spbt/html"]
+    (uf/delete-all-except-gitkeep md-dir)
+    (uf/delete-all-except-gitkeep html-dir)
+    (defp [ops (g/bind (g/set (gen-md-file md-dir) {:min-elements 1})
+                       gen-ops)]
+      (loop [model {} ops ops]
+        (if-let [op (first ops)]
+          (recur {} (rest ops))
+          true)) ;; pass test 
+      )))
 
 (comment
   (g/sample gen-md-text)
@@ -57,11 +91,13 @@
   (g/sample gen-valid-post-name)
   (g/sample gen-post-name)
   (g/sample gen-tags-and-frontmatter)
-  (g/sample (g/set (gen-md-files "test/fixture/spbt/md/")))
+  (g/sample (g/set (gen-md-file "test/fixture/spbt/md/")))
   (run! (fn [{:keys [path text]}] (spit path text))
         (-> "test/fixture/spbt/md/"
-            gen-md-files g/set g/sample last))
+            gen-md-file g/set g/sample last))
 
-  (def md-files (last (g/sample (g/set (gen-md-files "test/fixture/spbt/md/")))))
+  (def md-files (last (g/sample (g/set (gen-md-file "test/fixture/spbt/md/")))))
   (g/sample (gen-create md-files))
-  (g/sample (gen-ops md-files)))
+  (g/sample (gen-ops md-files))
+
+  (test-without-timing))
