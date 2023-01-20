430f5e7ff35d98c1d9801d39e3972fa75ae7f8d1
kur <rhdnfka94@gmail.com>
2023-01-20 12:16:48 +0900

    Add merge-and-assoc-happened

--- src/kur/blog/page/post/diff.clj
+++ src/kur/blog/page/post/diff.clj
@@ -1,8 +1,9 @@
 (ns kur.blog.page.post.diff
   "NOTE: state(post set) is created from file system directly."
-  (:require [clojure.set :as s]))
+  (:require [clojure.set :as s]
+            [kur.util.compare :refer [=by]]))
 
-(defn happened-assocd
+(defn ^:deprecated happened-assocd
   "old and new info(posts) from state sets mapped with same id"
   [old new]
   {:pre [(or old new) (not= old new)]}
@@ -10,7 +11,7 @@
         (nil? old) (assoc new :happened ::create)
         :else (assoc new :happened ::update))); (= (:id old) (:id new))
 
-(defn happened-assocds
+(defn ^:deprecated happened-assocds
   "Return changed posts associated happened event"
   [oldset newset]
   (let [;; Remove as-is (the intersection)
@@ -23,15 +24,52 @@
          (map (zipmap old-ids olds) ids)
          (map (zipmap new-ids news) ids))))
 
-(defn unchangeds [oldset newset]
+(defn ^:deprecated unchangeds [oldset newset]
   (s/intersection oldset newset))
 
-(comment
-  (def olds #{{:id 1}     {:id 3 :3 3} {:id 4}  {:id 2 :2  2} {:id 5}})
-  (def news     #{{:id 6} {:id 3 :3 3} {:id 4}  {:id 2 :2 -2} {:id 5 :5 -5}})
+(defn happened
+  [old now]
+  {:pre [(or old now)]}
+  (cond (nil? now) ::delete
+        (nil? old) ::create
+        (=by #(vector (:md-path %) ; fname change don't modify mtime..
+                      (:last-modified-millis %))
+             old now) ::as-is ;assume id are same
+        :else ::update))
 
-  (= (set (happened-assocds olds news))
+(defn merge-and-assoc-happened
+  "merge old posts and now posts, assoc happened.
+   It takes over 'loaded content' from old-posts to now-posts
+   - old-posts should be loaded(load-text post)
+   - now-posts should not be loaded"
+  [old-posts now-posts]
+  (let [old-ids (mapv :id old-posts)
+        now-ids (mapv :id now-posts)
+        ids (sort (distinct (into old-ids now-ids)))
+        mergeds (map #(assoc (merge %1 %2) :happened (happened %1 %2))
+                     (map (zipmap old-ids old-posts) ids)
+                     (map (zipmap now-ids now-posts) ids))]
+    mergeds
+    #_{:mergeds (map #(dissoc % :happened) mergeds)
+       :id:happened (zipmap ids (map :happened mergeds))}))
+
+(comment
+  (def olds #{{:id 1}
+              {:id 3 :3 3} {:id 4}
+              {:id 2 :last-modified-millis 1} {:id 5 :last-modified-millis 0}})
+  (def nows #{{:id 6}
+              {:id      3} {:id 4}
+              {:id 2 :last-modified-millis 20} {:id 5 :last-modified-millis 50}})
+
+  (= (set (happened-assocds olds nows))
      #{{:id 1, :happened ::delete}
        {:id 6, :happened ::create}
        {:id 2, :2 -2, :happened ::update}
-       {:id 5, :5 -5, :happened ::update}}))
+       {:id 5, :5 -5, :happened ::update}})
+
+  (happened nil 1)
+  (happened 1 nil)
+  (happened {:last-modified-millis 1} {:last-modified-millis 1})
+  (happened {:last-modified-millis 1} {:last-modified-millis 2})
+
+  (merge-and-assoc-happened olds nows))
