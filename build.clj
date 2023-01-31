3c0e6c2ff48cf3506871caa985f9846eed4617d2
kur <rhdnfka94@gmail.com>
2023-01-31 15:12:34 +0900

    Use version-independent jar name, Spit git version metadata

--- build.clj
+++ build.clj
@@ -1,32 +1,41 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.tools.build.api :as b]))
 
 (def target-dir "release/updater/target")
 (def class-dir (str target-dir "/" "classes"))
 (def src-dir (str target-dir "/" "src"))
 
 (def prog-name "updater")
-(def version "0.1.0")
-
+(def uber-file (format (str target-dir "/" "%s.jar") prog-name))
 (def basis (b/create-basis {:project "deps.edn"}))
-(def uber-file (format (str target-dir "/" "%s-%s-standalone.jar")
-                       prog-name version))
+
+;;
+(defn copy-lang-dir [lang]
+  (b/copy-dir {:src-dirs [(str "src" "/" lang)]
+               :target-dir (str src-dir "/" lang)}))
+
+(defn version [_]
+  {:tag        (b/git-process {:git-args ["describe" "--tags"]})
+   :hash       (b/git-process {:git-args ["rev-parse" "HEAD"]})
+   :short-hash (b/git-process {:git-args ["rev-parse" "--short" "HEAD"]})})
 
 ;; clj -T:build clean
 (defn clean [_]
   (b/delete {:path target-dir}))
 
-(defn copy-lang-dir [lang]
-  (b/copy-dir {:src-dirs [(str "src" "/" lang)]
-               :target-dir (str src-dir "/" lang)}))
-
 ;; clj -T:build uber
 (defn uber [_]
   (clean nil)
+
   (copy-lang-dir "js")
   #_(copy-lang-dir "html")
   #_(copy-lang-dir "css")
   (println "Copying File(s) success")
+
+  (spit (str target-dir "/" "version.edn")
+        (with-out-str (pprint (version nil))))
+  (println "Writing metadata success")
 
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
@@ -40,4 +49,6 @@
   (println "Building uberjar success")
 
   (b/delete {:path class-dir})
-  (println "Cleaning up.."))
+  (println "Cleaning up..")
+
+  (comment))
