6e7a68f7c7b8377f3e375f2da338fcb50e49a519
kur <rhdnfka94@gmail.com>
2023-01-31 14:44:51 +0900

    Copy src except clj, Print more info

--- build.clj
+++ build.clj
@@ -3,6 +3,7 @@
 
 (def target-dir "release/updater/target")
 (def class-dir (str target-dir "/" "classes"))
+(def src-dir (str target-dir "/" "src"))
 
 (def prog-name "updater")
 (def version "0.1.0")
@@ -15,15 +16,28 @@
 (defn clean [_]
   (b/delete {:path target-dir}))
 
+(defn copy-lang-dir [lang]
+  (b/copy-dir {:src-dirs [(str "src" "/" lang)]
+               :target-dir (str src-dir "/" lang)}))
+
 ;; clj -T:build uber
 (defn uber [_]
   (clean nil)
+  (copy-lang-dir "js")
+  #_(copy-lang-dir "html")
+  #_(copy-lang-dir "css")
+  (println "Copying File(s) success")
+
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
                   :class-dir class-dir})
   (println "Compilation success")
+
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis basis
            :main 'kur.blog.main})
-  (println "Building uberjar success"))
+  (println "Building uberjar success")
+
+  (b/delete {:path class-dir})
+  (println "Cleaning up.."))
