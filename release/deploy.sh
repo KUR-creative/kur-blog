3f490a2effe433f3507d304d68d8096d9dc06bc0
kur <rhdnfka94@gmail.com>
2023-02-01 11:00:45 +0900

    Add release dir and deploy script

---
+++ release/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copy the release directory to remote, 
+# and then run this script in remote ssh
+# Assume current working directory = release
+if [ `basename "$PWD"` != 'release' ]
+then
+    echo current working directory is not release
+    exit 1
+fi
+
+# Create /www (NOTE: could be extracted to a script)
+sudo mkdir -p /www/blog/html
+sudo mkdir -p /www/blog-base/md
+sudo chown -R $USER:$USER /www
+
+# Deploy updater
+cp -r updater/target/* /www/blog-base
+cp -r updater/config /www/blog-base/
+
+sudo mkdir /etc/systemd/user
+sudo cp updater/blog-updater.service /etc/systemd/user/
+systemctl --user daemon-reload 
+systemctl --user enable blog-updater.service
+systemctl --user start blog-updater.service
+
+# Deploy publisher
+sudo cp -r publisher/conf.d /etc/nginx/conf.d
+sudo cp -r publisher/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl enable nginx.service
+sudo systemctl start nginx.service
