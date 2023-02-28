8b95f8886ad617f02359e5abab0b29cce6e52423
kur <rhdnfka94@gmail.com>
2023-02-28 11:45:49 +0900

    Add regression test

---
+++ test/fixture/regression/156/md/meta-embed.md
@@ -0,0 +1,15 @@
+기본적으로 옵시디언의 임베딩 전략을 따라감.
+
+## video
+**모든 비디오 파일은 gif 대신 올림**(용량문제). 
+즉 무음`muted` 자동재생`autoplay` 반복`loop`
+소리가 필요하다면 `youtube`를 쓸 것. 
+
+### youtube embed
+다음 문법(simple embed 플러그인)으로 임베딩이 가능함
+`[text|embed](youtube-url)`
+[example|embed](https://www.youtube.com/watch?v=uk-UQf7d5UY)
+
+embed 안 되는 것
+그냥 url https://www.youtube.com/watch?v=uk-UQf7d5UY
+|embed 안 붙인 것 [example](https://www.youtube.com/watch?v=uk-UQf7d5UY)
---
+++ test/regression/kur/blog_test/oneshot_regression.clj
@@ -0,0 +1,27 @@
+(ns regression.kur.blog-test.oneshot-regression
+  "Test current updater generate the same htmls as a specific version
+   Test only initialization (one shot regression)"
+  (:require [babashka.fs :as fs]
+            [clojure.java.shell :refer [sh]]
+            [cprop.core :refer [load-config]]
+            [kur.blog.main :as main]
+            [kur.blog.monitor :as monitor]))
+
+(defn test-case [git-count]
+  (let [root-dir "test/fixture/regression"
+        cfg-path (format "%s/%s.edn" root-dir git-count)
+        expected-htmls-dir (format "%s/%s/html" root-dir git-count)]
+    (assert (fs/exists? cfg-path))
+    (assert (fs/exists? expected-htmls-dir))
+    {:cfg-path cfg-path, :expected-htmls-dir expected-htmls-dir}))
+
+(defn diff [git-count]
+  (let [{:keys [cfg-path expected-htmls-dir]} (test-case git-count)
+        {:keys [html-dir]} (load-config :file cfg-path)
+        updater (main/-main cfg-path)]
+    (monitor/stop! updater)
+    (sh "diff" html-dir expected-htmls-dir)))
+
+(comment
+  (def git-count 156)
+  (diff 156))
